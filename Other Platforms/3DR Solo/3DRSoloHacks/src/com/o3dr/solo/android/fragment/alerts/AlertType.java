d55b7155b362c02cc6531d1f911db2f2c52d5356
MrJabu <jajunkmail32@gmail.com>
2017-12-02 17:39:04 -0500

    Add files via upload

---
+++ Other Platforms/3DR Solo/3DRSoloHacks/src/com/o3dr/solo/android/fragment/alerts/AlertType.java
@@ -0,0 +1,8 @@
+class 
+{
+}
+
+/* Location:           /Users/kfinisterre/Desktop/Solo/3DRSoloHacks/unpacked_apk/classes_dex2jar.jar
+ * Qualified Name:     com.o3dr.solo.android.fragment.alerts.AlertType
+ * JD-Core Version:    0.6.2
+ */
